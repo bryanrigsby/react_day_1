f51e05fdd029b0cf08e880a53a84b70ca74aef23
Bryan Rigsby <bryanrigsby@att.net>
2024-05-15 18:33:02 -0500

    first commit

---
+++ react_day_one/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './componets/Navbar';
+import Body from './componets/Body';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Body />
+    </div>
+  );
+}
+
+export default App;
